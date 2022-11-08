c5cc39e12259371e687613fea2d9dd40bc5b228a
Rahmad Setiya Budi <rahmadsetiyabudi@gmail.com>
2022-11-08 13:51:47 +0700

    nambah model user sama migratenya

---
+++ app/Database/Migrations/2022-11-08-064034_Users.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class Users extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'id_user' => [
+                'type'           => 'INT',
+                'constraint'     => 5,
+                'unsigned'       => true,
+                'auto_increment' => true,
+            ],
+            'username' => [
+                'type'       => 'VARCHAR',
+                'constraint' => '100',
+            ],
+            'password' => [
+                'type' => 'VARCHAR',
+                'constraint' => '255',
+            ],
+            'role' => [
+                'type' => 'VARCHAR',
+                'constraint' => '255',
+            ],
+        ]);
+        $this->forge->addKey('id_user', true);
+        $this->forge->createTable('users');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('users');
+    }
+}
