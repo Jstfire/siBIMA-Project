20c62788b444109cab339f594f35eae760e667a4
Jstfire <mmahbubbillah@gmail.com>
2022-11-09 16:23:10 +0700

    mahbub8

--- app/Views/temp/header.php
+++ app/Views/temp/header.php
@@ -9,19 +9,19 @@
             $timezone = date("e");
             /* If the time is less than 1200 hours, show good morning */
             if ($time < "12") {
-                echo "Selamat Pagi ";
+                echo "Selamat Pagi, ";
             } else
             /* If the time is grater than or equal to 1200 hours, but less than 1700 hours, so good afternoon */
             if ($time >= "12" && $time < "16") {
-                echo "Selamat Siang ";
+                echo "Selamat Siang, ";
             } else
             /* Should the time be between or equal to 1700 and 1900 hours, show good evening */
             if ($time >= "16" && $time < "18") {
-                echo "Selamat Sore ";
+                echo "Selamat Sore, ";
             } else
             /* Finally, show good night if the time is greater than or equal to 1900 hours */
             if ($time >= "18") {
-                echo "Selamat Malam ";
+                echo "Selamat Malam, ";
             }
             echo $session->get('nama_tampil')."! | ".$session->get('role');
         ?>
--- app/Views/temp/sidebar.php
+++ app/Views/temp/sidebar.php
@@ -1,4 +1,5 @@
 <?php
+    $session = session();
     if(isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on')   
     $url = "https://";   
     else  
@@ -8,7 +9,7 @@
 
     // Append the requested resource location to the URL   
     $url.= $_SERVER['REQUEST_URI'];
-?>
+    ?>
 
 <body id="body-pd">
   <header class="header" id="header">
@@ -17,12 +18,12 @@
     </div>
     <div class="d-flex mt-1">
       <div class="me-3">
-        <p class="user-detail m-0">UPK</p>
-        <p class="user-detail m-0">Super Admin</p>
+        <p class="user-detail m-0 text-end"><?= $session->get('nama_tampil') ?></p>
+        <p class="user-detail m-0 text-end"><?= $session->get('role') ?></p>
       </div>
-      <div class="header_img">
+      <!-- <div class="header_img">
         <img src="https://i.imgur.com/hczKIze.jpg" alt="">
-      </div>
+      </div> -->
     </div>
   </header>
   <div class="l-navbar" id="nav-bar">
@@ -34,10 +35,27 @@
         </a>
         <div class="nav_list">
           <?php
+              if ($url == base_url().'/Admin/ProgresKegiatan') {
+                echo '
+                  <a href="'.base_url().'/Admin/ProgresKegiatan" class="nav_link active">
+                    <i class="bx bx-bar-chart-alt-2 nav_icon"></i>
+                    <span class="nav_name">Rangkuman Kegiatan</span>
+                  </a>
+                ';
+              } else {
+                echo '
+                  <a href="'.base_url().'/Admin/ProgresKegiatan" class="nav_link">
+                    <i class="bx bx-bar-chart-alt-2 nav_icon"></i>
+                    <span class="nav_name">Rangkuman Kegiatan</span>
+                  </a>
+                ';
+              }
+          ?>
+          <?php
               if ($url == base_url().'/Admin/TambahAkun') {
-                  echo '
-                  <a href="'.base_url().'/Admin/TambahAkun" class="nav_link active">
-                    <i class="bx bx-grid-alt nav_icon"></i>
+                echo '
+                <a href="'.base_url().'/Admin/TambahAkun" class="nav_link active">
+                <i class="bx bx-grid-alt nav_icon"></i>
                     <span class="nav_name">Akun Baru</span>
                   </a>
                   ';
@@ -84,23 +102,6 @@
                   ';
                 }
             ?>
-          <?php
-              if ($url == base_url().'/Admin/ProgresKegiatan') {
-                echo '
-                  <a href="'.base_url().'/Admin/ProgresKegiatan" class="nav_link active">
-                    <i class="bx bx-bar-chart-alt-2 nav_icon"></i>
-                    <span class="nav_name">Stats</span>
-                  </a>
-                ';
-              } else {
-                echo '
-                  <a href="'.base_url().'/Admin/ProgresKegiatan" class="nav_link">
-                    <i class="bx bx-bar-chart-alt-2 nav_icon"></i>
-                    <span class="nav_name">Stats</span>
-                  </a>
-                ';
-              }
-          ?>
         </div>
       </div>
       <a href="<?php echo base_url();?>" class="nav_link">
