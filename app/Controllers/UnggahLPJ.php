633134c2ea1eabe0119ed9550d966731fe296736
khairiyahny <76420075+khairiyahny@users.noreply.github.com>
2022-12-02 09:51:25 +0700

    Create UnggahLPJ.php

---
+++ app/Controllers/UnggahLPJ.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Controllers\BaseController;
+
+class UnggahLPJ extends BaseController
+{
+    public function index()
+    {
+        return view('unggahLPJ');
+    }
+}
