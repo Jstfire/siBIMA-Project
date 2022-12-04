57f3e4f1485cc6ade810f9793bc88ca21d3abcd2
Triana Nuramalia Shalli <117736578+shallidesu@users.noreply.github.com>
2022-12-04 15:59:48 +0700

    Update DashboardUPK.php

--- app/Controllers/DashboardUPK.php
+++ app/Controllers/DashboardUPK.php
@@ -4,6 +4,8 @@ namespace App\Controllers;
 
 use App\Controllers\BaseController;
 use App\Models\Proposal;
+use App\Models\Kegiatan;
+use App\Models\User;
 
 
 class DashboardUPK extends BaseController
