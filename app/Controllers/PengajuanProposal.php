f9bf51bb64d021c2ac8994044d6bd36b713386ae
Rahmad Setiya Budi <rahmadsetiyabudi@gmail.com>
2022-12-05 12:57:05 +0700

    proposal fix done

--- app/Controllers/PengajuanProposal.php
+++ app/Controllers/PengajuanProposal.php
@@ -72,8 +72,9 @@ class PengajuanProposal extends BaseController
         $fileProposal->move('proposal', $fileName);
 
         $this->proposal->insert($data);
+        $idProp = $this->proposal->where('link_proposal', $data['link_proposal'])->first();
+        $data['id_proposal'] = $idProp['id_proposal'];
         $this->kegiatan->insert($data);
-
         session()->setFlashdata('pesan', '<script>swal("Berhasil!", "Berhasil Mengajukan Proposal!", "success");</script>');
         return redirect()->to('DashboardBPH/ListProposal');
     }
@@ -81,5 +82,55 @@ class PengajuanProposal extends BaseController
     function download($id){
         $data = $this->proposal->find($id);
         return $this->response->download('proposal/'.$data['link_proposal'], null);
+    }
+
+    function detail($id)
+    {
+        $data['proposal'] = $this->proposal->where('proposal.id_proposal', $id)
+        ->join('kegiatan', 'proposal.id_proposal = kegiatan.id_proposal', 'left')
+        ->first();
+        return view('dashboardBPH/detail-proposal', $data);
+    }
+
+    function edit($id)
+    {
+        $data['proposal'] = $this->proposal->where('proposal.id_proposal', $id)->first();
+        $data['kegiatan'] = $this->kegiatan->where('kegiatan.id_proposal', $id)->first();
+        // dd($data);
+        return view('dashboardBPH/edit-proposal', $data);
+    }
+
+    function edit_act($id){
+        $data = $this->request->getPost();
+        $data['tanggal_kegiatan'] = Time::parse($data['tanggal_kegiatan'])->toDateTimeString();
+        $data['tahun_ajaran_kegiatan'] = '2022/2023';
+        $data['bulan_kegiatan'] = explode("-", $data['tanggal_kegiatan'])[1];
+        $data['id_ormawa'] = $this->user['id_ormawa'];
+        $data['jam_kegiatan'] = $data['jam_mulai'] . " " . $data["jam_akhir"];
+        if (isset($this->user['id_ukm'])) {
+            $data['id_ukm'] = $this->user['id_ukm'];
+        }
+        if (isset($this->user['id_bidang_divisi'])) {
+            $data['id_bidang_divisi'] = $this->user['id_bidang_divisi'];
+        }
+        $data['id_user'] = session()->get('id_user');
+        $fileProposal = $this->request->getFile('myPDF');
+        $fileName = $fileProposal->getName();
+        $data['link_proposal'] = $fileName;
+        $data['nama_proposal'] = $fileName;
+        $fileProposal->move('proposal', $fileName);
+
+        $this->proposal->update($id, $data);
+        $idProp = $this->proposal->where('link_proposal', $data['link_proposal'])->first();
+        $data['id_proposal'] = $idProp['id_proposal'];
+        $this->kegiatan->update($data['id_kegiatan'] ,$data);
+        session()->setFlashdata('pesan', '<script>swal("Berhasil!", "Berhasil Mengubah Proposal Kegiatan!", "success");</script>');
+        return redirect()->to('DashboardBPH/ListProposal');
+    }
+
+    function delete($id){
+        $this->proposal->delete($id);
+        session()->setFlashdata('pesan', '<script>swal("Berhasil!", "Berhasil Menghapus Proposal Kegiatan!", "success");</script>');
+        return redirect()->to('/DashboardBPH/ListProposal');
     }
 }
